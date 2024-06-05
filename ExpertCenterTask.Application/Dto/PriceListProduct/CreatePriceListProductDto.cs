6cf45531a693325a924f025acd3745f60f669c58
Hihiz <public@mail.ru>
2024-06-06 01:44:51 +0300

    Создать классы дто для продуктов прайс листа

---
+++ ExpertCenterTask.Application/Dto/PriceListProduct/CreatePriceListProductDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ExpertCenterTask.Application.Dto.PriceListProduct
+{
+    public class CreatePriceListProductDto
+    {
+        public int PriceListId { get; set; }
+        public int ProductId { get; set; }
+    }
+}
---
+++ ExpertCenterTask.Application/Dto/PriceListProduct/PriceListProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpertCenterTask.Application.Dto.PriceListProduct
+{
+    public class PriceListProductDto
+    {
+        public int PriceListId { get; set; }
+        public string PriceListTitle { get; set; }
+
+        public int ProductId { get; set; }
+        public string ProductTitle { get; set; }
+    }
+}
