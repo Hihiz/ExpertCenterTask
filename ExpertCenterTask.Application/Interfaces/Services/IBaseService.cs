db98a2327a2e12c3708f9c2fdbf69c2f1d9483ef
Hihiz <public@mail.ru>
2024-06-06 01:46:22 +0300

    Создать базовый сервис

---
+++ ExpertCenterTask.Application/Interfaces/Services/IBaseService.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpertCenterTask.Application.Interfaces.Services
+{
+    public interface IBaseService<T, TCreateDto, TUpdateDto>
+    {
+        Task<List<T>> GetAll(CancellationToken cancellationToken = default);
+        Task<T> GetById(int id, CancellationToken cancellationToken = default);
+        Task<T> Create(TCreateDto dto, CancellationToken cancellationToken = default);
+        Task<T> Update(int id, TUpdateDto dto, CancellationToken cancellationToken = default);
+        Task Delete(int id, CancellationToken cancellationToken = default);
+    }
+}
