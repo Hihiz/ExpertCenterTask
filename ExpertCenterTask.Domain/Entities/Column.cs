51179bf23f921a2ad4267ab240ab25b44c63e698
Hihiz <public@mail.ru>
2024-06-06 01:42:33 +0300

    Добавить свойство занчения

--- ExpertCenterTask.Domain/Entities/Column.cs
+++ ExpertCenterTask.Domain/Entities/Column.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public Domain.Enums.Type Type { get; set; }
+        public string Value { get; set; }
 
         public ICollection<PriceListColumn> PriceListColumns { get; } = new List<PriceListColumn>();
     }
