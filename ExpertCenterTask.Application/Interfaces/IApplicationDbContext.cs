03caff55b03ad1c95f9b134e4057ffb4e7bfadb1
Hihiz <public@mail.ru>
2024-06-06 01:47:03 +0300

    Создать интерфейс класса контекста

---
+++ ExpertCenterTask.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ExpertCenterTask.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpertCenterTask.Application.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<PriceList> PriceLists { get; set; }
+        DbSet<Product> Products { get; set; }
+        DbSet<Column> Columns { get; set; }
+        DbSet<PriceListProduct> PriceListProducts { get; set; }
+        DbSet<PriceListColumn> PriceListColumns { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
