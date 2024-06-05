67361490042c858c8cb206264b10c1b2f5f8c18b
Hihiz <public@mail.ru>
2024-06-06 01:46:40 +0300

    Создать сервис для прайс листа

---
+++ ExpertCenterTask.Application/Interfaces/Services/IPriceListService.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpertCenterTask.Application.Interfaces.Services
+{
+    public interface IPriceListService<T, TDetailDto, TCreateDto, TUpdateDto>
+    {
+        Task<List<T>> GetAll(CancellationToken cancellationToken = default);
+        Task<TDetailDto> GetById(int id, CancellationToken cancellationToken = default);
+        Task<T> Create(TCreateDto dto, CancellationToken cancellationToken = default);
+        Task<T> Update(int id, TUpdateDto dto, CancellationToken cancellationToken = default);
+        Task Delete(int id, CancellationToken cancellationToken = default);
+    }
+}
