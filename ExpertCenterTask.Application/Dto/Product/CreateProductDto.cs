537f4d2637f5ccef6dfcf1eed4ed65aa5a3956e0
Hihiz <public@mail.ru>
2024-06-06 01:45:06 +0300

    Создать классы дто для продукта

---
+++ ExpertCenterTask.Application/Dto/Product/CreateProductDto.cs
@@ -0,0 +1,12 @@
+﻿using ExpertCenterTask.Application.Dto.Column;
+
+namespace ExpertCenterTask.Application.Dto.Product
+{
+    public class CreateProductDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List</*ColumnCollectionDto*/ColumnDto> Columns { get; set; }
+
+    }
+}
---
+++ ExpertCenterTask.Application/Dto/Product/ProductDto.cs
@@ -0,0 +1,12 @@
+﻿using ExpertCenterTask.Application.Dto.Column;
+
+namespace ExpertCenterTask.Application.Dto.Product
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Arcticle { get; set; }
+        public List</*ColumnCollectionDto*/ColumnDto> Columns { get; set; }
+    }
+}
