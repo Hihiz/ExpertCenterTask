fa808c8a9996ee9d9409777ee1a6886abe5d836f
Hihiz <public@mail.ru>
2024-06-06 20:31:13 +0300

    Регистрация зависимостей

---
+++ ExpertCenterTask.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,39 @@
+﻿using ExpertCenterTask.Application.Interfaces;
+using ExpertCenterTask.Application.Interfaces.Repositories;
+using ExpertCenterTask.Domain.Entities;
+using ExpertCenterTask.Infrastructure.Data;
+using ExpertCenterTask.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExpertCenterTask.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>((sp, options) =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+            services.RepositoriesInit();
+
+            return services;
+        }
+
+        public static void RepositoriesInit(this IServiceCollection services)
+        {
+            services.AddScoped<IBaseRepository<PriceList>, PriceListRepository>();
+            services.AddScoped<IBaseRepository<Product>, ProductRepository>();
+            services.AddScoped<IBaseRepository<Column>, ColumnRepository>();
+            services.AddScoped<IBaseRepository<PriceListColumn>, PriceListColumnRepository>();
+            services.AddScoped<IBaseRepository<PriceListProduct>, PriceListProductRepository>();
+        }
+    }
+}
